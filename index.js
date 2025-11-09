e7da1c74ca78ba6854b51aad892eb6f4703203d7
MonkeTGG <dylanstas@hotmail.com>
2025-11-09 17:23:56 +1030

    added an example window along with window moving.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
+startMenu.style.display = "block";
+window.style.display = "block";
 
 function toggleElementVisibility(objectId) {
     var startMenu = document.getElementById(objectId);
@@ -9,3 +11,31 @@ function toggleElementVisibility(objectId) {
     }
 }
 
+function makeWindowDraggable(objectId) {
+    var objectTitleBar = document.getElementById(objectId.concat("TitleBar"));
+    var window = document.getElementById(objectId);
+    let windowMoving = false;
+    let offsetX, offsetY;
+    let startX, startY
+    window.style.left = "20px";
+    window.style.top = "20px";
+    
+    objectTitleBar.addEventListener("mousedown", function(event) {
+        windowMoving = true;
+        offsetX = event.clientX;
+        offsetY = event.clientY;
+        startX = parseInt(window.style.left);
+        startY = parseInt(window.style.top);
+    });
+
+    objectTitleBar.addEventListener("mousemove", function(event) {
+        if (windowMoving === true) {
+            window.style.left = startX + event.clientX - offsetX + "px";
+            window.style.top = startY + event.clientY + - offsetY + "px";
+        }
+    });
+
+    objectTitleBar.addEventListener("mouseup", function(event) {
+        windowMoving = false;
+    });
+}
